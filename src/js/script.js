28b36b090dcede10a473e9f1001cefadbe0c57ee
Arthur Souza <arthur_vitor16@outlook.com>
2018-01-14 12:28:22 -0300

    Implementando lazy scroll

--- src/js/script.js
+++ src/js/script.js
@@ -1,17 +1,27 @@
-(function (win, doc) {
-  // toggle menu
-  var buttonMenu = doc.querySelector('[data-toggle-menu]')
-  buttonMenu.addEventListener('click', function(e) {
-    var menu = doc.querySelector(this.getAttribute('data-toggle-menu'))
+jQuery(function () {
+  // Toggle menu
+  jQuery('[data-toggle-menu]').click(function(e) {
+    e.preventDefault()
+    var menu = jQuery(jQuery(this).attr('data-toggle-menu'))
 
-    menu.classList.toggle('isActive')
+    menu.toggleClass('isActive')
+  })
+
+  // Next Section
+  jQuery('[data-next-section]').click(function(event) {
+    event.preventDefault()
+    var button = jQuery(this)
+    var section = jQuery('[data-section="'+ button.attr('data-next-section') + '"]').offset().top
+
+    jQuery('html, body').animate({
+      scrollTop: section
+    }, 500)
   })
 
   // show newsletter error
-  var form = doc.querySelector('.section-five__form')
+  var form = document.querySelector('.section-five__form')
   form.addEventListener('submit', function(e) {
     e.preventDefault()
     this.querySelector('.section-five__form__errors').classList.add('isActive')
   })
-
-})(window, document)
+})
